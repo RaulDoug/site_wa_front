1ffdd5c93ae7ed1d08c5f32ab0ddfd8fa0616fd9
Raul <rauldougbr@gmail.com>
2026-07-13 15:51:07 -0300

    Variaveis de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Instância com as configurações bases
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 });
 
 // Interceptador para injetar o token JWT automaticamente
